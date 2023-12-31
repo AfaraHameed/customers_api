a2b3328eeebd9d973c5c51dd1514073750e077bf
AfaraHameed <afarasidheek@gmail.com>
2023-12-31 08:22:07 +0530

    connected to mongodb and created route file for customers

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const app = express()
+require('dotenv').config()
+const customersRoutes = require('./routes/customers')
+
+const PORT = process.env.PORT || 3000
+const DB_CONN_STRING = process.env.DATABASE_URL
+
+mongoose.connect(DB_CONN_STRING)
+const database = mongoose.connection;
+database.on('error',(error)=>{
+console.log("DB error",error);
+})
+
+database.once('connected',()=>{
+    console.log("Database connected");
+})
+
+app.use(express.json())
+
+// routes middleware
+app.use('/api/customers',customersRoutes)
+app.listen(PORT,()=>{
+    console.log(`Server is running on ${PORT}`)
+})
